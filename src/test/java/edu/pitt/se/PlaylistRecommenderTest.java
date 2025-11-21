cde1b64383e5c0ef188fdc3a20b13f8aeb9ff897
Joshua David Frank <17joshuafrank@gmail.com>
2025-11-21 18:10:28 -0500

    added other test cases

--- src/test/java/edu/pitt/se/PlaylistRecommenderTest.java
+++ src/test/java/edu/pitt/se/PlaylistRecommenderTest.java
@@ -12,4 +12,17 @@ public class PlaylistRecommenderTest {
         List<Integer> highBPMs = List.of(150, 160, 170);
         assertEquals("HIGH", PlaylistRecommender.classifyEnergy(highBPMs));
     }
+
+    @Test
+    void testIsValidTrackTitle() {
+        assertTrue(PlaylistRecommender.isValidTrackTitle("Song Title 123"));
+        assertFalse(PlaylistRecommender.isValidTrackTitle("Song@Title!"));
+    }
+
+    @Test
+    void testNormalizeVolume() {
+        assertEquals(0, PlaylistRecommender.normalizeVolume(-10));
+        assertEquals(100, PlaylistRecommender.normalizeVolume(150));
+        assertEquals(50, PlaylistRecommender.normalizeVolume(50));
+    }
 }
