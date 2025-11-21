58436532d861d2d7b355ae63672f347db0487cfa
Joshua David Frank <17joshuafrank@gmail.com>
2025-11-21 18:02:58 -0500

    added ci.yaml file

--- src/test/java/edu/pitt/se/PlaylistRecommenderTest.java
+++ src/test/java/edu/pitt/se/PlaylistRecommenderTest.java
@@ -3,10 +3,13 @@ package edu.pitt.se;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 public class PlaylistRecommenderTest {
 
     @Test
-    public void placeholder() {
-        assertTrue(true);
+    void testClassifyEnergy() {
+        List<Integer> highBPMs = List.of(150, 160, 170);
+        assertEquals("HIGH", PlaylistRecommender.classifyEnergy(highBPMs));
     }
 }
